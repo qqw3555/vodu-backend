94aec34dfaae3d2bd7097ba221a66c672d8d8a10
qqw3555 <qqwweerr0987654321.aa@gmail.com>
2026-09-13 01:02:44 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -23,6 +23,6 @@ def get_movies():
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
-if name == 'main':
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
